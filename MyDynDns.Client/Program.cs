854a74b492ba5d024aa60175be7de0ed3f2021d1
Christopher Maneu <christopher@maneu.net>
2021-02-04 15:42:12 +0100

    Add project files.

---
+++ MyDynDns.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyDynDns.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("My DynDNS Updater. Starting update.");
+
+            HttpClient httpClient = new HttpClient();
+
+            int autoUpdateFrequency = int.Parse(Environment.GetEnvironmentVariable("AUTOPUDATE_REFRESH")) *1000;
+
+            do
+            {
+                await httpClient.GetAsync(Environment.GetEnvironmentVariable("AUTOUPDATE_ENDPOINT"));
+                Console.WriteLine($"Status updated on {DateTime.Now:G}");
+                await Task.Delay(autoUpdateFrequency);
+            } while (true);
+        }
+    }
+}
