58311e1f4a6ba979cf215f7efb65f49883c13bad
Christopher Maneu <christopher@maneu.net>
2021-02-04 17:45:16 +0100

    Add server update to cloudflare

--- MyDynDns.Server/UpdateFunction.cs
+++ MyDynDns.Server/UpdateFunction.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -20,7 +23,35 @@ namespace MyDynDns.Server
         {
             log.LogInformation($"Update request for {hostname}");
 
+            string zoneIdentifier = null;
+            string recordIdentifier = null;
+
+            zoneIdentifier = req.Query[nameof(zoneIdentifier)];
+            recordIdentifier = req.Query[nameof(recordIdentifier)];
+
+            if (string.IsNullOrWhiteSpace(zoneIdentifier) || string.IsNullOrWhiteSpace(recordIdentifier))
+                return new BadRequestResult();
+
+            string content = GetIpFromRequestHeaders(req);
+
+            log.LogInformation($"Updating {hostname} from {content}");
+
+            var cloudFlare = new CloudflareClient(Environment.GetEnvironmentVariable("CLOUDFLARE_APITOKEN"));
+
+            await cloudFlare.UpdateDnsZone(hostname, zoneIdentifier, recordIdentifier, content);
             return new OkObjectResult("ok");
+        }
+
+        private static string GetIpFromRequestHeaders(HttpRequest request)
+        {
+            string clientIp = (request.Headers["X-Forwarded-For"].FirstOrDefault() ?? "").Split(new char[] { ':' }).FirstOrDefault();
+
+            if (string.IsNullOrWhiteSpace(clientIp))
+            {
+                clientIp = request.HttpContext.Connection.RemoteIpAddress.ToString();
+            }
+
+            return clientIp;
         }
     }
 }
